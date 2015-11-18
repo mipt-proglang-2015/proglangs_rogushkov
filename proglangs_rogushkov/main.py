d062fbfbb619021f1b084325a04c03f6342baede
FizikRoot <rogushkov@gmail.com>
2015-11-18 04:26:42 +0300

    1

--- proglangs_rogushkov/main.py
+++ proglangs_rogushkov/main.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     a = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(100))
     b = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(100))
     start = time.process_time()
-    rc.lcs(a,b)
+    lcs.lcs(a,b)
     end = time.process_time()
     delta = end - start
     print("Time when using vector: {:.4f} ms".format(delta * 1000))
 
     start = time.process_time()
-    rc.lcs_map(a,b)
+    lcs.lcs_map(a,b)
     end = time.process_time()
     delta = end - start
     print("Time with using map and string: {:.4f} ms".format(delta * 1000))
